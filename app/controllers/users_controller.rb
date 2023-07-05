d9153a33314bb3122823a92946ca2fd523d19d20
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-07-05 13:17:50 +0000

    4-17

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
   end
   
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
   
   private
